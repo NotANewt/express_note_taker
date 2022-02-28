89c1e3ba485c3f52090b0df0f1c6e80a6784df70
NotANewt <meegafauna@gmail.com>
2022-02-27 20:44:14 -0600

    worked on creating html and api routes

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
+// import express and path packages
 const express = require("express");
 const path = require("path");
 
+// initialize the app variable by setting it to the value of express()
 const app = express();
+
+// set PORT variable
 const PORT = process.env.PORT || 3001;
 
 const api = require("./public/assets/js/index");
 
-// TODO: Modify for this app
-app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
-
 // TODO: create HTML route: GET /notes should return the notes.html file.
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
 
 // TODO: create HTML route: GET * should return the index file
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 
 // TODO: create API route: GET /api/notes should read the db.json file and return all saved notes as JSON.
+// Require the JSON file and assign it to a variable called 'notesData'
+const notesData = require("./db/db.json");
+
+// res.json() allows us to return JSON instead of a buffer, string, or static file
+app.get("/api/notes", (req, res) => res.json(notesData));
 
 // TODO: create API route: POST /api/notes should receive a new note to save on the request body, add it
 // to the db.json file, and then return the new note to the client.
@@ -25,3 +33,6 @@ app.listen(PORT, () => console.log(`Example app listening at http://localhost:${
 //     DELETE /api/notes/:id should receive a query parameter that contains the id of a note to
 //     delete. To delete a note, you'll need to read all notes from the db.json file, remove the
 //     note with the given id property, and then rewrite the notes to the db.json file.
+
+// TODO: Modify for this app
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
