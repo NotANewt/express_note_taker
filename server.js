aa1521a0065a440958482a2f6713446895bb6e15
NotANewt <meegafauna@gmail.com>
2022-02-27 20:25:00 -0600

    updated pseudocode for clarity

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
 const express = require("express");
-const api = require("./public/assets/js/index");
-
-const PORT = process.env.PORT || 3001;
+const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// TODO: GET Route for notes page
+const api = require("./public/assets/js/index");
 
-// TODO: POST Route for notes page
+// TODO: Modify for this app
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
 
-// TODO: DELETE Route for notes page
+// TODO: create HTML route: GET /notes should return the notes.html file.
 
-/*
-The following HTML routes should be created:
-    GET /notes should return the notes.html file.
-    GET * should return the index.html file.
+// TODO: create HTML route: GET * should return the index file
 
-The following API routes should be created:
-    GET /api/notes should read the db.json file and return all saved notes as JSON.
-    POST /api/notes should receive a new note to save on the request body, add it 
-    to the db.json file, and then return the new note to the client. 
-    You'll need to find a way to give each note a unique id when it's saved 
-    (look into npm packages that could do this for you).
+// TODO: create API route: GET /api/notes should read the db.json file and return all saved notes as JSON.
 
-Bonus
-    You haven’t learned how to handle DELETE requests, but this application offers that 
-    functionality on the front end. As a bonus, try to add the DELETE route to the application 
-    using the following guideline:
+// TODO: create API route: POST /api/notes should receive a new note to save on the request body, add it
+// to the db.json file, and then return the new note to the client.
+// You'll need to find a way to give each note a unique id when it's saved
+// (look into npm packages that could do this for you).
 
-        DELETE /api/notes/:id should receive a query parameter that contains the id of a note to 
-        delete. To delete a note, you'll need to read all notes from the db.json file, remove the 
-        note with the given id property, and then rewrite the notes to the db.json file.
-*/
+// TODO BONUS: create API route: add the DELETE route to the application
+// using the following guideline:
+//     DELETE /api/notes/:id should receive a query parameter that contains the id of a note to
+//     delete. To delete a note, you'll need to read all notes from the db.json file, remove the
+//     note with the given id property, and then rewrite the notes to the db.json file.
